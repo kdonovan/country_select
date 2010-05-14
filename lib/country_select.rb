fb64af63670de52c55069708c580b228180f639f
Kali Donovan <kali.donovan@gmail.com>
2010-05-14 13:04:09 -0700

    Updated to store ISO country codes

--- lib/country_select.rb
+++ lib/country_select.rb
@@ -17,77 +17,70 @@ module ActionView
         if priority_countries
           priority_countries = priority_countries.collect do |code|
             index = COUNTRIES_CODES.index(code)
-            [COUNTRIES_NAMES[index], index + 1]
+            next unless index
+            [COUNTRIES_NAMES[index], code]
           end
           
           country_options += options_for_select(priority_countries, selected)
           country_options += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
         end
 
-        return country_options + options_for_select(COUNTRIES_OPTIONS, selected)
+        return country_options + options_for_select(COUNTRIES, selected)
       end
       # All the countries included in the country_options output.
-      COUNTRIES = [
-        ["Afghanistan", "af"], ["Aland Islands", "ax"], ["Albania", "al"], ["Algeria", "dz"], 
-        ["American Samoa", "as"], ["Andorra", "ad"], ["Angola", "ao"], ["Anguilla", "ai"], 
-        ["Antarctica", "aq"], ["Antigua And Barbuda", "ag"], ["Argentina", "ar"], ["Armenia", "am"], 
-        ["Aruba", "aw"], ["Australia", "au"], ["Austria", "at"], ["Azerbaijan", "az"], ["Bahamas", "bs"], 
-        ["Bahrain", "bh"], ["Bangladesh", "bd"], ["Barbados", "bb"], ["Belarus", "by"], ["Belgium", "be"], 
-        ["Belize", "bz"], ["Benin", "bj"], ["Bermuda", "bm"], ["Bhutan", "bt"], ["Bolivia", "bo"], 
-        ["Bosnia And Herzegovina", "ba"], ["Botswana", "bw"], ["Bouvet Island", "bv"], ["Brazil", "br"], 
-        ["British Indian Ocean Territory", "io"], ["Brunei Darussalam", "bn"], ["Bulgaria", "bg"], 
-        ["Burkina Faso", "bf"], ["Burundi", "bi"], ["Cambodia", "kh"], ["Cameroon", "cm"], ["Canada", "ca"], 
-        ["Cape Verde", "cv"], ["Cayman Islands", "ky"], ["Central African Republic", "cf"], ["Chad", "td"], 
-        ["Chile", "cl"], ["China", "cn"], ["Christmas Island", "cx"], ["Cocos (Keeling) Islands", "cc"], 
-        ["Colombia", "co"], ["Comoros", "km"], ["Congo", "cg"], ["Congo, The Democratic Republic Of The", "cd"], 
-        ["Cook Islands", "ck"], ["Costa Rica", "cr"], ["Cote D'ivoire", "ci"], ["Croatia", "hr"], ["Cuba", "cu"], 
-        ["Cyprus", "cy"], ["Czech Republic", "cz"], ["Denmark", "dk"], ["Djibouti", "dj"], ["Dominica", "dm"], 
-        ["Dominican Republic", "do"], ["Ecuador", "ec"], ["Egypt", "eg"], ["El Salvador", "sv"], 
-        ["Equatorial Guinea", "gq"], ["Eritrea", "er"], ["Estonia", "ee"], ["Ethiopia", "et"], 
-        ["Falkland Islands (Malvinas)", "fk"], ["Faroe Islands", "fo"], ["Fiji", "fj"], ["Finland", "fi"], 
-        ["France", "fr"], ["French Guiana", "gf"], ["French Polynesia", "pf"], ["French Southern Territories", "tf"], 
-        ["Gabon", "ga"], ["Gambia", "gm"], ["Georgia", "ge"], ["Germany", "de"], ["Ghana", "gh"], ["Gibraltar", "gi"], 
-        ["Greece", "gr"], ["Greenland", "gl"], ["Grenada", "gd"], ["Guadeloupe", "gp"], ["Guam", "gu"], 
-        ["Guatemala", "gt"], ["Guernsey", "gg"], ["Guinea", "gn"], ["Guinea Bissau", "gw"], ["Guyana", "gy"], 
-        ["Haiti", "ht"], ["Heard Island And Mcdonald Islands", "hm"], ["Holy See (Vatican City State)", "va"], 
-        ["Honduras", "hn"], ["Hong Kong", "hk"], ["Hungary", "hu"], ["Iceland", "is"], ["India", "in"], 
-        ["Indonesia", "id"], ["Iran, Islamic Republic Of", "ir"], ["Iraq", "iq"], ["Ireland", "ie"], 
-        ["Isle Of Man", "im"], ["Israel", "il"], ["Italy", "it"], ["Jamaica", "jm"], ["Japan", "jp"], 
-        ["Jersey", "je"], ["Jordan", "jo"], ["Kazakhstan", "kz"], ["Kenya", "ke"], ["Kiribati", "ki"], 
-        ["Korea, Democratic People's Republic Of", "kp"], ["Korea, Republic Of", "kr"], ["Kuwait", "kw"], 
-        ["Kyrgyzstan", "kg"], ["Lao People's Democratic Republic", "la"], ["Latvia", "lv"], ["Lebanon", "lb"], 
-        ["Lesotho", "ls"], ["Liberia", "lr"], ["Libyan Arab Jamahiriya", "ly"], ["Liechtenstein", "li"], 
-        ["Lithuania", "lt"], ["Luxembourg", "lu"], ["Macao", "mo"], ["Macedonia, The Former Yugoslav Republic Of", "mk"], 
-        ["Madagascar", "mg"], ["Malawi", "mw"], ["Malaysia", "my"], ["Maldives", "mv"], ["Mali", "ml"], ["Malta", "mt"], 
-        ["Marshall Islands", "mh"], ["Martinique", "mq"], ["Mauritania", "mr"], ["Mauritius", "mu"], ["Mayotte", "yt"], 
-        ["Mexico", "mx"], ["Micronesia, Federated States Of", "fm"], ["Moldova, Republic Of", "md"], ["Monaco", "mc"], 
-        ["Mongolia", "mn"], ["Montenegro", "me"], ["Montserrat", "ms"], ["Morocco", "ma"], ["Mozambique", "mz"], 
-        ["Myanmar", "mm"], ["Namibia", "na"], ["Nauru", "nr"], ["Nepal", "np"], ["Netherlands", "nl"], 
-        ["Netherlands Antilles", "an"], ["New Caledonia", "nc"], ["New Zealand", "nz"], ["Nicaragua", "ni"], 
-        ["Niger", "ne"], ["Nigeria", "ng"], ["Niue", "nu"], ["Norfolk Island", "nf"], ["Northern Mariana Islands", "mp"], 
-        ["Oman", "om"], ["Pakistan", "pk"], ["Palau", "pw"], ["Palestinian Territory, Occupied", "ps"], ["Panama", "pa"], 
-        ["Papua New Guinea", "pg"], ["Paraguay", "py"], ["Peru", "pe"], ["Philippines", "ph"], ["Pitcairn", "pn"], 
-        ["Poland", "pl"], ["Portugal", "pt"], ["Puerto Rico", "pr"], ["Qatar", "qa"], ["Reunion", "re"], ["Romania", "ro"], 
-        ["Russian Federation", "ru"], ["Rwanda", "rw"], ["Saint Barthelemy", "bl"], ["Saint Helena", "sh"], 
-        ["Saint Kitts And Nevis", "kn"], ["Saint Lucia", "lc"], ["Saint Martin", "mf"], ["Saint Pierre And Miquelon", "pm"], 
-        ["Saint Vincent And The Grenadines", "vc"], ["Samoa", "ws"], ["San Marino", "sm"], ["Sao Tome And Principe", "st"], 
-        ["Saudi Arabia", "sa"], ["Senegal", "sn"], ["Serbia", "rs"], ["Seychelles", "sc"], ["Sierra Leone", "sl"], 
-        ["Singapore", "sg"], ["Slovakia", "sk"], ["Slovenia", "si"], ["Solomon Islands", "sb"], ["Somalia", "so"], 
-        ["South Africa", "za"], ["South Georgia And The South Sandwich Islands", "gs"], ["Spain", "es"], ["Sri Lanka", "lk"], 
-        ["Sudan", "sd"], ["Suriname", "sr"], ["Svalbard And Jan Mayen", "sj"], ["Swaziland", "sz"], ["Sweden", "se"], 
-        ["Switzerland", "ch"], ["Syrian Arab Republic", "sy"], ["Taiwan", "tw"], ["Tajikistan", "tj"], 
-        ["Tanzania, United Republic Of", "tz"], ["Thailand", "th"], ["Timor Leste", "tl"], ["Togo", "tg"], ["Tokelau", "tk"], 
-        ["Tonga", "to"], ["Trinidad And Tobago", "tt"], ["Tunisia", "tn"], ["Turkey", "tr"], ["Turkmenistan", "tm"], 
-        ["Turks And Caicos Islands", "tc"], ["Tuvalu", "tv"], ["Uganda", "ug"], ["Ukraine", "ua"], 
-        ["United Arab Emirates", "ae"], ["United Kingdom", "gb"], ["United States", "us"], 
-        ["United States Minor Outlying Islands", "um"], ["Uruguay", "uy"], ["Uzbekistan", "uz"], ["Vanuatu", "vu"], 
-        ["Venezuela", "ve"], ["Viet Nam", "vn"], ["Virgin Islands, British", "vg"], ["Virgin Islands, U.S.", "vi"], 
-        ["Wallis And Futuna", "wf"], ["Western Sahara", "eh"], ["Yemen", "ye"], ["Zambia", "zm"], ["Zimbabwe", "zw"]
-      ] unless const_defined?("COUNTRIES")
+      unless const_defined?("COUNTRIES")
+        COUNTRIES = [
+          ["Afghanistan", "AF"], ["Aland Islands", "AX"], ["Albania", "AL"], ["Algeria", "DZ"], ["American Samoa", "AS"],
+          ["Andorra", "AD"], ["Angola", "AO"], ["Anguilla", "AI"], ["Antarctica", "AQ"], ["Antigua And Barbuda", "AG"], 
+          ["Argentina", "AR"], ["Armenia", "AM"], ["Aruba", "AW"], ["Australia", "AU"], ["Austria", "AT"], ["Azerbaijan", "AZ"], 
+          ["Bahamas", "BS"], ["Bahrain", "BH"], ["Bangladesh", "BD"], ["Barbados", "BB"], ["Belarus", "BY"], ["Belgium", "BE"], 
+          ["Belize", "BZ"], ["Benin", "BJ"], ["Bermuda", "BM"], ["Bhutan", "BT"], ["Bolivia", "BO"], ["Bosnia And Herzegovina", "BA"],
+          ["Botswana", "BW"], ["Bouvet Island", "BV"], ["Brazil", "BR"], ["British Indian Ocean Territory", "IO"], ["Brunei Darussalam", "BN"],
+          ["Bulgaria", "BG"], ["Burkina Faso", "BF"], ["Burundi", "BI"], ["Cambodia", "KH"], ["Cameroon", "CM"], ["Canada", "CA"], 
+          ["Cape Verde", "CV"], ["Cayman Islands", "KY"], ["Central African Republic", "CF"], ["Chad", "TD"], ["Chile", "CL"], 
+          ["China", "CN"], ["Christmas Island", "CX"], ["Cocos (Keeling) Islands", "CC"], ["Colombia", "CO"], ["Comoros", "KM"], 
+          ["Congo", "CG"], ["Congo, The Democratic Republic Of The", "CD"], ["Cook Islands", "CK"], ["Costa Rica", "CR"], 
+          ["Cote D'ivoire", "CI"], ["Croatia", "HR"], ["Cuba", "CU"], ["Cyprus", "CY"], ["Czech Republic", "CZ"], ["Denmark", "DK"],
+          ["Djibouti", "DJ"], ["Dominica", "DM"], ["Dominican Republic", "DO"], ["Ecuador", "EC"], ["Egypt", "EG"], ["El Salvador", "SV"],
+          ["Equatorial Guinea", "GQ"], ["Eritrea", "ER"], ["Estonia", "EE"], ["Ethiopia", "ET"], ["Falkland Islands (Malvinas)", "FK"], 
+          ["Faroe Islands", "FO"], ["Fiji", "FJ"], ["Finland", "FI"], ["France", "FR"], ["French Guiana", "GF"], ["French Polynesia", "PF"],
+          ["French Southern Territories", "TF"], ["Gabon", "GA"], ["Gambia", "GM"], ["Georgia", "GE"], ["Germany", "DE"], ["Ghana", "GH"],
+          ["Gibraltar", "GI"], ["Greece", "GR"], ["Greenland", "GL"], ["Grenada", "GD"], ["Guadeloupe", "GP"], ["Guam", "GU"], 
+          ["Guatemala", "GT"], ["Guernsey", "GG"], ["Guinea", "GN"], ["Guinea Bissau", "GW"], ["Guyana", "GY"], ["Haiti", "HT"], 
+          ["Heard Island And Mcdonald Islands", "HM"], ["Holy See (Vatican City State)", "VA"], ["Honduras", "HN"], ["Hong Kong", "HK"],
+          ["Hungary", "HU"], ["Iceland", "IS"], ["India", "IN"], ["Indonesia", "ID"], ["Iran, Islamic Republic Of", "IR"], ["Iraq", "IQ"],
+          ["Ireland", "IE"], ["Isle Of Man", "IM"], ["Israel", "IL"], ["Italy", "IT"], ["Jamaica", "JM"], ["Japan", "JP"], ["Jersey", "JE"],
+          ["Jordan", "JO"], ["Kazakhstan", "KZ"], ["Kenya", "KE"], ["Kiribati", "KI"], ["Korea, Democratic People's Republic Of", "KP"],
+          ["Korea, Republic Of", "KR"], ["Kuwait", "KW"], ["Kyrgyzstan", "KG"], ["Lao People's Democratic Republic", "LA"], ["Latvia", "LV"],
+          ["Lebanon", "LB"], ["Lesotho", "LS"], ["Liberia", "LR"], ["Libyan Arab Jamahiriya", "LY"], ["Liechtenstein", "LI"], 
+          ["Lithuania", "LT"], ["Luxembourg", "LU"], ["Macao", "MO"], ["Macedonia, The Former Yugoslav Republic Of", "MK"], 
+          ["Madagascar", "MG"], ["Malawi", "MW"], ["Malaysia", "MY"], ["Maldives", "MV"], ["Mali", "ML"], ["Malta", "MT"], 
+          ["Marshall Islands", "MH"], ["Martinique", "MQ"], ["Mauritania", "MR"], ["Mauritius", "MU"], ["Mayotte", "YT"], ["Mexico", "MX"],
+          ["Micronesia, Federated States Of", "FM"], ["Moldova, Republic Of", "MD"], ["Monaco", "MC"], ["Mongolia", "MN"], 
+          ["Montenegro", "ME"], ["Montserrat", "MS"], ["Morocco", "MA"], ["Mozambique", "MZ"], ["Myanmar", "MM"], ["Namibia", "NA"], 
+          ["Nauru", "NR"], ["Nepal", "NP"], ["Netherlands", "NL"], ["Netherlands Antilles", "AN"], ["New Caledonia", "NC"], 
+          ["New Zealand", "NZ"], ["Nicaragua", "NI"], ["Niger", "NE"], ["Nigeria", "NG"], ["Niue", "NU"], ["Norfolk Island", "NF"], 
+          ["Northern Mariana Islands", "MP"], ["Oman", "OM"], ["Pakistan", "PK"], ["Palau", "PW"], ["Palestinian Territory, Occupied", "PS"],
+          ["Panama", "PA"], ["Papua New Guinea", "PG"], ["Paraguay", "PY"], ["Peru", "PE"], ["Philippines", "PH"], ["Pitcairn", "PN"],
+          ["Poland", "PL"], ["Portugal", "PT"], ["Puerto Rico", "PR"], ["Qatar", "QA"], ["Reunion", "RE"], ["Romania", "RO"], 
+          ["Russian Federation", "RU"], ["Rwanda", "RW"], ["Saint Barthelemy", "BL"], ["Saint Helena", "SH"], ["Saint Kitts And Nevis", "KN"],
+          ["Saint Lucia", "LC"], ["Saint Martin", "MF"], ["Saint Pierre And Miquelon", "PM"], ["Saint Vincent And The Grenadines", "VC"],
+          ["Samoa", "WS"], ["San Marino", "SM"], ["Sao Tome And Principe", "ST"], ["Saudi Arabia", "SA"], ["Senegal", "SN"], ["Serbia", "RS"],
+          ["Seychelles", "SC"], ["Sierra Leone", "SL"], ["Singapore", "SG"], ["Slovakia", "SK"], ["Slovenia", "SI"], ["Solomon Islands", "SB"],
+          ["Somalia", "SO"], ["South Africa", "ZA"], ["South Georgia And The South Sandwich Islands", "GS"], ["Spain", "ES"], 
+          ["Sri Lanka", "LK"], ["Sudan", "SD"], ["Suriname", "SR"], ["Svalbard And Jan Mayen", "SJ"], ["Swaziland", "SZ"], ["Sweden", "SE"],
+          ["Switzerland", "CH"], ["Syrian Arab Republic", "SY"], ["Taiwan", "TW"], ["Tajikistan", "TJ"], 
+          ["Tanzania, United Republic Of", "TZ"], ["Thailand", "TH"], ["Timor Leste", "TL"], ["Togo", "TG"], ["Tokelau", "TK"], 
+          ["Tonga", "TO"], ["Trinidad And Tobago", "TT"], ["Tunisia", "TN"], ["Turkey", "TR"], ["Turkmenistan", "TM"], 
+          ["Turks And Caicos Islands", "TC"], ["Tuvalu", "TV"], ["Uganda", "UG"], ["Ukraine", "UA"], ["United Arab Emirates", "AE"], 
+          ["United Kingdom", "GB"], ["United States", "US"], ["United States Minor Outlying Islands", "UM"], ["Uruguay", "UY"], 
+          ["Uzbekistan", "UZ"], ["Vanuatu", "VU"], ["Venezuela", "VE"], ["Viet Nam", "VN"], ["Virgin Islands, British", "VG"], 
+          ["Virgin Islands, U.S.", "VI"], ["Wallis And Futuna", "WF"], ["Western Sahara", "EH"], ["Yemen", "YE"], ["Zambia", "ZM"],
+          ["Zimbabwe", "ZW"]]
+      end
       
       COUNTRIES_NAMES = COUNTRIES.collect {|c| c[0] }
       COUNTRIES_CODES = COUNTRIES.collect {|c| c[1] }
-      COUNTRIES_OPTIONS = COUNTRIES.to_enum(:each_with_index).collect {|c, i| [c[0], i + 1] }
     end
     
     class InstanceTag
